924ef1bb727a0857f350e8ff67601fd83422ff03
Katip <Katip@DESKTOP-6P2FP43>
2021-03-24 17:10:18 +0300

    FinalProject-Frontend

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import{HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
 import { ListResponseModel } from '../models/listResponseModel';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -23,4 +24,9 @@ export class ProductService {
     let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
+
+  add(product:Product):Observable<ResponseModel>{
+   return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add",product)
+  }
+ 
 }
